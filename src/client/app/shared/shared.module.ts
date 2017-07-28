343d5c8dc7104b995c64281ac42cbc459e997d8d
piiamiia <orav.piia@gmail.com>
2017-07-28 18:04:09 +0300

    First commit with UI

---
+++ src/client/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  imports: [CommonModule, RouterModule],
+  declarations: [NavbarComponent, FooterComponent],
+  exports: [NavbarComponent, FooterComponent,
+    CommonModule, FormsModule, RouterModule]
+})
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: []
+    };
+  }
+}
